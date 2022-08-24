66409c15b486b09e49f2e2aff92b8e92ae0c9bf6
Vincent Fiduccia <vincent@acorn.io>
2022-08-24 15:49:58 -0700

    Algolia search
    Signed-off-by: Vincent Fiduccia <vincent@acorn.io>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -95,6 +95,12 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['cue','docker'],
       },
+      algolia: {
+        appId: '7QCEFR54LA',
+        apiKey: '0091e059262804a95d3253d28bc90eeb',
+        indexName: 'acorn-io',
+
+      }
     }),
 };
 
