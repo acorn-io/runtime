c13501c0a7c7778b1ba29857f7188079dd2db54a
Joshua Silverio <joshua@acorn.io>
2022-10-18 12:48:42 -0700

    fix: Refactor err msg to include commands to paste
    Signed-off-by: Joshua Silverio <joshua@acorn.io>

--- pkg/controller/appdefinition/namespace.go
+++ pkg/controller/appdefinition/namespace.go
@@ -49,9 +49,11 @@ func ensureNamespaceOwned(req router.Request, appInstance *v1.AppInstance) error
 	if ns.Labels[labels.AcornAppNamespace] != appInstance.Namespace ||
 		ns.Labels[labels.AcornAppName] != appInstance.Name {
 		return fmt.Errorf("can not use namespace %s, existing namespace must have labels "+
-			"[acorn.io/app-namespace=%s]"+" and [acorn.io/app-name=%s]."+
-			" And namespace will be deleted when the app is deleted",
-			appInstance.Spec.TargetNamespace, appInstance.Namespace, appInstance.Name)
+			"acorn.io/app-namespace"+" and acorn.io/app-name (Apply Using: kubectl label namespaces %s acorn.io/app-name=%s "+
+			"kubectl label namespaces %s acorn.io/app-namespace=acorn) "+
+			"Namespace will be deleted when the app is deleted",
+			appInstance.Spec.TargetNamespace, appInstance.Spec.TargetNamespace, appInstance.Name,
+			appInstance.Spec.TargetNamespace)
 	}
 	return nil
 }
