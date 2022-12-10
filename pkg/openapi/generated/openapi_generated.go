62b64ef9b00842f2b414006856dfeb24fffb1136
Thorsten Klein <tk@thklein.io>
2022-12-10 07:19:24 +0100

    fix: generate api spec for publishprotocol
    Signed-off-by: Thorsten Klein <tk@thklein.io>

--- pkg/openapi/generated/openapi_generated.go
+++ pkg/openapi/generated/openapi_generated.go
@@ -3488,6 +3488,12 @@ func schema_pkg_apis_internalacornio_v1_Endpoint(ref common.ReferenceCallback) c
 							Format: "",
 						},
 					},
+					"publishProtocol": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
 					"pending": {
 						SchemaProps: spec.SchemaProps{
 							Type:   []string{"boolean"},
