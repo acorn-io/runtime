a1d5d90c6a1b3f390798dda97c2ae8c91e27d6f1
Grant Linville <grant@acorn.io>
2023-03-10 17:17:59 -0500

    Add NetworkPolicy tests and fix info tests
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/controller/appdefinition/networkpolicy_test.go
+++ pkg/controller/appdefinition/networkpolicy_test.go
@@ -1,11 +1,51 @@
 package appdefinition
 
 import (
+	"github.com/acorn-io/acorn/pkg/config"
 	"github.com/acorn-io/acorn/pkg/scheme"
+	"github.com/acorn-io/baaah/pkg/router"
 	"github.com/acorn-io/baaah/pkg/router/tester"
 	"testing"
 )
 
 func TestNetworkPolicy(t *testing.T) {
-	tester.DefaultTest(t, scheme.Scheme, "testdata/networkpolicy", NetworkPolicy)
+	tester.DefaultTest(t, scheme.Scheme, "testdata/networkpolicy/default", NetworkPolicy)
+}
+
+func TestNetworkPolicyWithIngressNamespace(t *testing.T) {
+	tester.DefaultTest(t, scheme.Scheme, "testdata/networkpolicy/ingressnamespace", func(req router.Request, resp router.Response) error {
+		conf, err := config.Get(req.Ctx, req.Client)
+		if err != nil {
+			return err
+		}
+
+		conf.IngressControllerNamespace = toStringPointer("nginx")
+		err = config.Set(req.Ctx, req.Client, conf)
+		if err != nil {
+			return err
+		}
+
+		return NetworkPolicy(req, resp)
+	})
+}
+
+func TestNetworkPolicyWithNodeCIDR(t *testing.T) {
+	tester.DefaultTest(t, scheme.Scheme, "testdata/networkpolicy/nodecidr", func(req router.Request, resp router.Response) error {
+		conf, err := config.Get(req.Ctx, req.Client)
+		if err != nil {
+			return err
+		}
+
+		conf.NodeCIDR = toStringPointer("10.2.0.1/24")
+		err = config.Set(req.Ctx, req.Client, conf)
+		if err != nil {
+			return err
+		}
+
+		return NetworkPolicy(req, resp)
+	})
+}
+
+func toStringPointer(input string) *string {
+	return &input
 }
