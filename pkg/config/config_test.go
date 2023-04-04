f1e4b3d5aa03fc4064a89aa5931a5dfab59f44f0
tylerslaton <mtslaton1@gmail.com>
2023-04-04 14:21:51 -0400

    Fix faulty merging and update tests for Config
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -96,100 +96,124 @@ func TestAcornDNSStates(t *testing.T) {
 
 func TestMergeConfigWithZeroLengthStringsArrayShouldNotOverride(t *testing.T) {
 	oldConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{"foo"},
 		AllowUserAnnotations:        []string{"foo"},
 		AllowUserLabels:             []string{"foo"},
 		PropagateProjectLabels:      []string{"foo"},
 		PropagateProjectAnnotations: []string{"foo"},
 		ClusterDomains:              []string{".acorn.io"},
+		ServiceLBAnnotations:        []string{"foo"},
 	}
 
 	newConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{},
 		AllowUserAnnotations:        []string{},
 		AllowUserLabels:             []string{},
 		PropagateProjectLabels:      []string{},
 		PropagateProjectAnnotations: []string{},
 		ClusterDomains:              []string{},
+		ServiceLBAnnotations:        []string{},
 	}
 
 	result := merge(oldConfig, newConfig)
+	assert.Equal(t, []string{"foo"}, result.AllowTrafficFromNamespace)
 	assert.Equal(t, []string{"foo"}, result.AllowUserAnnotations)
 	assert.Equal(t, []string{"foo"}, result.AllowUserLabels)
 	assert.Equal(t, []string{"foo"}, result.PropagateProjectAnnotations)
 	assert.Equal(t, []string{"foo"}, result.PropagateProjectLabels)
 	assert.Equal(t, []string{".acorn.io"}, result.ClusterDomains)
+	assert.Equal(t, []string{"foo"}, result.ServiceLBAnnotations)
 }
 
 func TestMergeConfigWithActualValueStringsArrayShouldOverride(t *testing.T) {
 	oldConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{"foo"},
 		AllowUserAnnotations:        []string{"foo"},
 		AllowUserLabels:             []string{"foo"},
 		PropagateProjectLabels:      []string{"foo"},
 		PropagateProjectAnnotations: []string{"foo"},
 		ClusterDomains:              []string{".acorn.io"},
+		ServiceLBAnnotations:        []string{"foo"},
 	}
 
 	newConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{"bar"},
 		AllowUserAnnotations:        []string{"bar"},
 		AllowUserLabels:             []string{"bar"},
 		PropagateProjectLabels:      []string{"bar"},
 		PropagateProjectAnnotations: []string{"bar", "brah"},
 		ClusterDomains:              []string{"bar.acorn.io"},
+		ServiceLBAnnotations:        []string{"bar"},
 	}
 
 	result := merge(oldConfig, newConfig)
+	assert.Equal(t, []string{"bar"}, result.AllowTrafficFromNamespace)
 	assert.Equal(t, []string{"bar"}, result.AllowUserAnnotations)
 	assert.Equal(t, []string{"bar"}, result.AllowUserLabels)
 	assert.Equal(t, []string{"bar"}, result.PropagateProjectLabels)
 	assert.Equal(t, []string{"bar", "brah"}, result.PropagateProjectAnnotations)
 	assert.Equal(t, []string{".bar.acorn.io"}, result.ClusterDomains)
+	assert.Equal(t, []string{"bar"}, result.ServiceLBAnnotations)
 }
 
 func TestMergeConfigWithNilStringsArrayShouldNotOverride(t *testing.T) {
 	oldConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{"foo"},
 		AllowUserAnnotations:        []string{"foo"},
 		AllowUserLabels:             []string{"foo"},
 		PropagateProjectLabels:      []string{"foo"},
 		PropagateProjectAnnotations: []string{"foo"},
 		ClusterDomains:              []string{".acorn.io"},
+		ServiceLBAnnotations:        []string{"foo"},
 	}
 
 	newConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   nil,
 		AllowUserAnnotations:        nil,
 		AllowUserLabels:             nil,
 		PropagateProjectLabels:      nil,
 		PropagateProjectAnnotations: nil,
 		ClusterDomains:              nil,
+		ServiceLBAnnotations:        nil,
 	}
 
 	result := merge(oldConfig, newConfig)
+	assert.Equal(t, []string{"foo"}, result.AllowTrafficFromNamespace)
 	assert.Equal(t, []string{"foo"}, result.AllowUserAnnotations)
 	assert.Equal(t, []string{"foo"}, result.AllowUserLabels)
 	assert.Equal(t, []string{"foo"}, result.PropagateProjectLabels)
 	assert.Equal(t, []string{"foo"}, result.PropagateProjectAnnotations)
 	assert.Equal(t, []string{".acorn.io"}, result.ClusterDomains)
+	assert.Equal(t, []string{"foo"}, result.ServiceLBAnnotations)
 }
 
 func TestMergeConfigWithEmptyStringStringsArrayShouldOverrideToNil(t *testing.T) {
 	oldConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{"foo"},
 		AllowUserAnnotations:        []string{"foo"},
 		AllowUserLabels:             []string{"foo"},
 		PropagateProjectLabels:      []string{"foo"},
 		PropagateProjectAnnotations: []string{"foo"},
 		ClusterDomains:              []string{".acorn.io"},
+		ServiceLBAnnotations:        []string{"foo=bar"},
 	}
 
 	newConfig := &apiv1.Config{
+		AllowTrafficFromNamespace:   []string{""},
 		AllowUserAnnotations:        []string{""},
 		AllowUserLabels:             []string{""},
 		PropagateProjectLabels:      []string{""},
 		PropagateProjectAnnotations: []string{""},
 		ClusterDomains:              []string{""},
+		ServiceLBAnnotations:        []string{""},
 	}
 
 	result := merge(oldConfig, newConfig)
+	assert.Nil(t, result.AllowTrafficFromNamespace)
 	assert.Nil(t, result.AllowUserAnnotations)
 	assert.Nil(t, result.AllowUserLabels)
 	assert.Nil(t, result.PropagateProjectLabels)
 	assert.Nil(t, result.PropagateProjectAnnotations)
 	assert.Nil(t, result.ClusterDomains)
+	assert.Nil(t, result.ServiceLBAnnotations)
 }
