76e7d0a4d4423ca6fec57a385727aca644e5b32e
Thorsten Klein <tk@thklein.io>
2023-04-12 23:12:23 +0200

    fix: only consider current image when checking appinstance against imageAllowRules (#1428) (#1476)
    * fix: only consider current image when checking appinstance against imageallowrules
    
    Signed-off-by: Thorsten Klein <tk@thklein.io>
    
    * fix: CheckImageAllowed handler to only operate on current image
    
    Before, it could check the updated (but same tagged) remote image and
    thus throw an error into the status field.
    
    Also, now we pass the digest through earlier to enable skipping a call
    to the external registry.
    
    Signed-off-by: Thorsten Klein <tk@thklein.io>
    
    ---------
    
    Signed-off-by: Thorsten Klein <tk@thklein.io>

--- pkg/controller/appdefinition/checkimageallowed.go
+++ pkg/controller/appdefinition/checkimageallowed.go
@@ -4,34 +4,39 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	v1 "github.com/acorn-io/acorn/pkg/apis/internal.acorn.io/v1"
 	"github.com/acorn-io/acorn/pkg/condition"
 	"github.com/acorn-io/acorn/pkg/imageallowrules"
 	"github.com/acorn-io/baaah/pkg/router"
+	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/sirupsen/logrus"
 )
 
 // CheckImageAllowedHandler is a router handler that checks if the image is allowed by the image allow rules and sets a status field accordingly
+// This is only working on the currently specified image, referenced by digest, to avoid false positives (alerts) if the remote image has been updated
 func CheckImageAllowedHandler(transport http.RoundTripper) router.HandlerFunc {
 	return func(req router.Request, resp router.Response) error {
 		appInstance := req.Object.(*v1.AppInstance)
 		cond := condition.Setter(appInstance, resp, v1.AppInstanceConditionImageAllowed)
 
-		targetImage, unknownReason := determineTargetImage(appInstance)
-		if targetImage == "" {
-			if appInstance.Status.AppImage.Name != "" {
-				targetImage = appInstance.Status.AppImage.Name
-			} else {
-				if unknownReason != "" {
-					cond.Unknown(unknownReason)
-				} else {
-					cond.Error(fmt.Errorf("no image specified"))
-				}
-				return nil
-			}
+		// We're only checking against the currently used image, so if the image name or digest is empty, we can't check
+		if appInstance.Status.AppImage.Name == "" || appInstance.Status.AppImage.Digest == "" {
+			cond.Unknown("")
+			return nil
 		}
+
+		ref, err := name.ParseReference(appInstance.Status.AppImage.Name, name.WithDefaultRegistry(""), name.WithDefaultTag(""))
+		if err != nil {
+			e := fmt.Errorf("failed to parse image name: %w", err)
+			logrus.Error(e)
+			cond.Error(e)
+			return nil
+		}
+
+		targetImage := ref.Context().Digest(strings.TrimPrefix(appInstance.Status.AppImage.Digest, "sha256:")).Name()
 
 		if err := imageallowrules.CheckImageAllowed(req.Ctx, req.Client, appInstance.Namespace, targetImage, remote.WithTransport(transport)); err != nil {
 			if errors.Is(err, &imageallowrules.ErrImageNotAllowed{}) {
