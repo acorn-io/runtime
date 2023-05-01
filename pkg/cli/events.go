5a17b7bb5f45ab41b64aa989b3c28b3c9ff37629
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-05-01 15:42:30 -0400

    WIP: add events cmd wiring
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

---
+++ pkg/cli/events.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	// "fmt"
+
+	cli "github.com/acorn-io/acorn/pkg/cli/builder"
+	// "github.com/acorn-io/acorn/pkg/client"
+	// "github.com/acorn-io/acorn/pkg/imagesource"
+	// "github.com/acorn-io/acorn/pkg/progressbar"
+	// "github.com/google/go-containerregistry/pkg/name"
+	// "github.com/rancher/wrangler/pkg/merr"
+	"github.com/spf13/cobra"
+)
+
+func NewEvents(c CommandContext) *cobra.Command {
+	cmd := cli.Command(&Events{client: c.ClientFactory}, cobra.Command{
+		Use: "events [flags]",
+		// TODO(njhale): Add examples
+		Example: `
+# List Acorn events.
+`,
+		SilenceUsage: true,
+		Short:        "List Acorn events",
+		// TODO(njhale): better long description
+		Long: "List events about acorn resources",
+		// Args: cobra.MinimumNArgs(1),
+	})
+	// cmd.Flags().SetInterspersed(false)
+	return cmd
+}
+
+type Events struct {
+	// TODO(njhale): Add flags/options
+	// Push     bool     `usage:"Push image after build"`
+	// File     string   `short:"f" usage:"Name of the build file (default \"DIRECTORY/Acornfile\")"`
+	// Tag      []string `short:"t" usage:"Apply a tag to the final build"`
+	// Platform []string `short:"p" usage:"Target platforms (form os/arch[/variant][:osversion] example linux/amd64)"`
+	// Profile  []string `usage:"Profile to assign default values"`
+	client ClientFactory
+}
+
+func (s *Events) Run(cmd *cobra.Command, args []string) error {
+	// TODO(njhale): Implement me
+	return nil
+}
---
+++ pkg/cli/events_test.go
@@ -0,0 +1,3 @@
+package cli
+
+// TODO(njhale): Add tests
