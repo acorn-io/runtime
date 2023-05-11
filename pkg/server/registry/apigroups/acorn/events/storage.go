60f514f115a6db6d5933f06920d164dbc1b5a59a
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-05-11 15:26:54 -0400

    Add event storage strategy decorator

--- pkg/server/registry/apigroups/acorn/events/storage.go
+++ pkg/server/registry/apigroups/acorn/events/storage.go
@@ -1,14 +1,22 @@
 package events
 
 import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"hash/fnv"
+	"strings"
+
 	apiv1 "github.com/acorn-io/acorn/pkg/apis/api.acorn.io/v1"
 	v1 "github.com/acorn-io/acorn/pkg/apis/internal.acorn.io/v1"
-	// "github.com/acorn-io/acorn/pkg/client"
 	"github.com/acorn-io/acorn/pkg/publicname"
 	"github.com/acorn-io/acorn/pkg/tables"
 	"github.com/acorn-io/mink/pkg/stores"
+	"github.com/acorn-io/mink/pkg/strategy"
 	"github.com/acorn-io/mink/pkg/strategy/remote"
 	"github.com/acorn-io/mink/pkg/strategy/translation"
+	"github.com/acorn-io/mink/pkg/types"
+	apierror "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apiserver/pkg/registry/rest"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -19,10 +27,9 @@ func NewStorage(c kclient.WithWatch) rest.Storage {
 		&translator{},
 		remote.NewRemote(&v1.EventInstance{}, c),
 	)
-	// strategy := &Strategy{
-	// 	delegate: publicname.NewStrategy(remoteResource),
-	// }
-	strategy := publicname.NewStrategy(remoteResource)
+	strategy := &eventStrategy{
+		CompleteStrategy: publicname.NewStrategy(remoteResource),
+	}
 
 	// Events are immutable, so Update is not supported.
 	// Events can't be deleted directly, they are automatically GCed after
@@ -36,4 +43,70 @@ func NewStorage(c kclient.WithWatch) rest.Storage {
 		// WithWatch(strategy).
 		WithCompleteCRUD(strategy).
 		Build()
+}
+
+type eventStrategy struct {
+	strategy.CompleteStrategy
+}
+
+func (s *eventStrategy) Create(ctx context.Context, obj types.Object) (types.Object, error) {
+	latest := obj.(*apiv1.Event)
+
+	// Set the event's name to a deterministic ID generated from its content
+	id, err := eventID(latest)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to generate event ID: [%w]", err)
+	}
+	latest.SetName(id)
+
+	// TODO(njhale): Reject on max-count (either here or at a lower level; e.g. storage layer)
+	created, err := s.CompleteStrategy.Create(ctx, latest)
+	if err == nil {
+		// Success! Bail out early
+		return created, err
+	}
+
+	if !apierror.IsAlreadyExists(err) {
+		return nil, err
+	}
+
+	// An event already exists with this ID, let's add an observation to it instead
+	current, err := s.CompleteStrategy.Get(ctx, latest.Namespace, latest.Name)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get current event observation: [%w]", err)
+	}
+
+	updated, err := s.CompleteStrategy.Update(ctx, addObservation(latest, current.(*apiv1.Event)))
+	if err != nil {
+		return nil, fmt.Errorf("Failed to add latest event observation: [%w]", err)
+	}
+
+	return updated, nil
+}
+
+func eventID(e *apiv1.Event) (string, error) {
+	// TODO: Reduce the field set used to generate when composite events are added.
+
+	// TODO(njhale): Find a better way of selecting and encoding field sets. Maybe a multi-layered io.Writer.
+	fieldSet := strings.Join([]string{
+		e.Type,
+		string(e.Severity),
+		e.Actor,
+		e.Subject.String(),
+		e.Description,
+		e.Observed.String(),
+	}, "")
+	h := fnv.New128a()
+	if _, err := h.Write([]byte(fieldSet)); err != nil {
+		return "", err
+	}
+
+	digest := h.Sum([]byte{})
+
+	return hex.EncodeToString(digest), nil
+}
+
+func addObservation(from, to *apiv1.Event) *apiv1.Event {
+	// TODO: Implement me when composite events are added.
+	return to
 }
---
+++ pkg/server/registry/apigroups/acorn/events/storage_test.go
@@ -0,0 +1,67 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	apiv1 "github.com/acorn-io/acorn/pkg/apis/api.acorn.io/v1"
+	"github.com/acorn-io/acorn/pkg/apis/internal.acorn.io/v1"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestEventID(t *testing.T) {
+	for _, tt := range []struct {
+		name  string
+		equal bool
+		a, b  apiv1.Event
+	}{
+		{
+			name:  "Equal/Duplicate",
+			equal: true,
+			a:     apiv1.Event{},
+			b:     apiv1.Event{},
+		},
+		{
+			name:  "Equal/Diff/Context",
+			equal: true,
+			a: apiv1.Event{
+				Context: v1.GenericMap{
+					"info": "1",
+				},
+			},
+			b: apiv1.Event{
+				Context: v1.GenericMap{
+					"info": 1,
+				},
+			},
+		},
+		{
+			name:  "NotEqual/Diff/Observed",
+			equal: false,
+			a: apiv1.Event{
+				Observed: metav1.Now(),
+			},
+			b: apiv1.Event{
+				Observed: metav1.NewTime(metav1.Now().Add(time.Hour)),
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			idA, err := eventID(&tt.a)
+			require.NoError(t, err)
+
+			idB, err := eventID(&tt.b)
+			require.NoError(t, err)
+
+			if tt.equal {
+				// IDs should match
+				require.Equal(t, idA, idB)
+				return
+			}
+			// IDs shouldn't match
+			require.NotEqual(t, idA, idB)
+		})
+
+	}
+}
