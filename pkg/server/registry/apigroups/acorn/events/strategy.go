6d8a6a983a0b2834cbb443fc84b10a4569d54a35
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-05-18 15:47:58 -0400

    Simplify event listing
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/server/registry/apigroups/acorn/events/strategy.go
+++ pkg/server/registry/apigroups/acorn/events/strategy.go
@@ -2,13 +2,11 @@ package events
 
 import (
 	"context"
-	"sort"
 	"strconv"
 
 	apiv1 "github.com/acorn-io/acorn/pkg/apis/api.acorn.io/v1"
 	"github.com/acorn-io/mink/pkg/strategy"
 	"github.com/acorn-io/mink/pkg/types"
-	"github.com/sirupsen/logrus"
 	"k8s.io/apiserver/pkg/storage"
 )
 
@@ -19,7 +17,6 @@ type eventStrategy struct {
 func (s *eventStrategy) List(ctx context.Context, namespace string, opts storage.ListOptions) (types.ObjectList, error) {
 	// Unmarshal custom field selectors and strip them from the list options before
 	// passing to lower-level strategies (that don't support them).
-	// TODO(njhale): I'm sure there's a better way to (un)marshal these
 	var q query
 	stripped, err := opts.Predicate.Field.Transform(func(f, v string) (string, string, error) {
 		var err error
@@ -36,11 +33,7 @@ func (s *eventStrategy) List(ctx context.Context, namespace string, opts storage
 		return nil, err
 	}
 	opts.Predicate.Field = stripped
-	logrus.Warnf("query: [%v]", q)
 
-	// TODO(njhale): Filtering like this adds an extra O(n*lgn) time and O(n) space to every List call.
-	// That's not great, and might be a sign that this is the wrong level of abstraction to filter at.
-	// How hard would it be to move this to the storage layer?
 	full, err := s.CompleteStrategy.List(ctx, namespace, opts)
 	if err != nil {
 		return nil, err
@@ -56,25 +49,13 @@ type query struct {
 }
 
 func (q query) on(list *apiv1.EventList) (*apiv1.EventList, error) {
-	// TODO: This can definitely be made more time-efficient and is not a "pure" function; i.e. it causes side-effects by mutating list
-	sort.Slice(list.Items, func(i, j int) bool {
-		return list.Items[i].Observed.Before(&list.Items[j].Observed)
-	})
-
-	result := make([]apiv1.Event, 0, len(list.Items))
-	for _, event := range list.Items {
-		if len(result) == cap(result) {
-			break
-		}
-
-		if !q.details {
-			event.Details = nil
-		}
-
-		result = append(result, event)
+	if q.details {
+		return list, nil
 	}
-
-	list.Items = result // TODO(njhale): Will this cause an inconsistent metav1.ListMeta?
+	for i, event := range list.Items {
+		event.Details = nil
+		list.Items[i] = event
+	}
 
 	return list, nil
 }
