aac93951ba1d8d281361f5196b875881f61a8725
Grant Linville <grant@acorn.io>
2023-06-05 21:19:19 -0400

    Create separate UpdateArgs struct in order to remove --name from the update command
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/cli/update.go
+++ pkg/cli/update.go
@@ -24,8 +24,30 @@ func NewUpdate(c CommandContext) *cobra.Command {
 	return cmd
 }
 
+type UpdateArgs struct {
+	Region          string   `usage:"Region in which to deploy the app, immutable"`
+	File            string   `short:"f" usage:"Name of the build file (default \"DIRECTORY/Acornfile\")"`
+	Volume          []string `usage:"Bind an existing volume (format existing:vol-name,field=value) (ex: pvc-name:app-data)" short:"v" split:"false"`
+	Secret          []string `usage:"Bind an existing secret (format existing:sec-name) (ex: sec-name:app-secret)" short:"s"`
+	Link            []string `usage:"Link external app as a service in the current app (format app-name:container-name)"`
+	PublishAll      *bool    `usage:"Publish all (true) or none (false) of the defined ports of application" short:"P"`
+	Publish         []string `usage:"Publish port of application (format [public:]private) (ex 81:80)" short:"p"`
+	Profile         []string `usage:"Profile to assign default values"`
+	Env             []string `usage:"Environment variables to set on running containers" short:"e"`
+	Label           []string `usage:"Add labels to the app and the resources it creates (format [type:][name:]key=value) (ex k=v, containers:k=v)" short:"l"`
+	Annotation      []string `usage:"Add annotations to the app and the resources it creates (format [type:][name:]key=value) (ex k=v, containers:k=v)"`
+	Dangerous       bool     `usage:"Automatically approve all privileges requested by the application"`
+	Output          string   `usage:"Output API request without creating app (json, yaml)" short:"o"`
+	TargetNamespace string   `usage:"The name of the namespace to be created and deleted for the application resources"`
+	NotifyUpgrade   *bool    `usage:"If true and the app is configured for auto-upgrades, you will be notified in the CLI when an upgrade is available and must confirm it"`
+	AutoUpgrade     *bool    `usage:"Enabled automatic upgrades."`
+	Interval        string   `usage:"If configured for auto-upgrade, this is the time interval at which to check for new releases (ex: 1h, 5m)"`
+	Memory          []string `usage:"Set memory for a workload in the format of workload=memory. Only specify an amount to set all workloads. (ex foo=512Mi or 512Mi)" short:"m"`
+	ComputeClass    []string `usage:"Set computeclass for a workload in the format of workload=computeclass. Specify a single computeclass to set all workloads. (ex foo=example-class or example-class)"`
+}
+
 type Update struct {
-	RunArgs
+	UpdateArgs
 	Image          string `usage:"Acorn image name"`
 	ConfirmUpgrade bool   `usage:"When an auto-upgrade app is marked as having an upgrade available, pass this flag to confirm the upgrade. Used in conjunction with --notify-upgrade."`
 	Pull           bool   `usage:"Re-pull the app's image, which will cause the app to re-deploy if the image has changed"`
@@ -57,8 +79,6 @@ func (s *Update) Run(cmd *cobra.Command, args []string) error {
 	name := args[0]
 	args = args[1:]
 
-	s.RunArgs.Name = name
-
 	if s.ConfirmUpgrade && s.Pull {
 		return fmt.Errorf("only --confirm-upgrade or --pull can be set at once")
 	}
@@ -82,7 +102,7 @@ func (s *Update) Run(cmd *cobra.Command, args []string) error {
 	}
 
 	r := Run{
-		RunArgs: s.RunArgs,
+		RunArgs: s.getRunArgs(name),
 		Wait:    s.Wait,
 		Quiet:   s.Quiet,
 		Update:  true,
@@ -90,4 +110,29 @@ func (s *Update) Run(cmd *cobra.Command, args []string) error {
 		client:  s.client,
 	}
 	return r.Run(cmd, append([]string{s.Image}, args...))
+}
+
+func (s *Update) getRunArgs(name string) RunArgs {
+	return RunArgs{
+		Name:            name,
+		Region:          s.UpdateArgs.Region,
+		File:            s.UpdateArgs.File,
+		Volume:          s.UpdateArgs.Volume,
+		Secret:          s.UpdateArgs.Secret,
+		Link:            s.UpdateArgs.Link,
+		PublishAll:      s.UpdateArgs.PublishAll,
+		Publish:         s.UpdateArgs.Publish,
+		Profile:         s.UpdateArgs.Profile,
+		Env:             s.UpdateArgs.Env,
+		Label:           s.UpdateArgs.Label,
+		Annotation:      s.UpdateArgs.Annotation,
+		Dangerous:       s.UpdateArgs.Dangerous,
+		Output:          s.UpdateArgs.Output,
+		TargetNamespace: s.UpdateArgs.TargetNamespace,
+		NotifyUpgrade:   s.UpdateArgs.NotifyUpgrade,
+		AutoUpgrade:     s.UpdateArgs.AutoUpgrade,
+		Interval:        s.UpdateArgs.Interval,
+		Memory:          s.UpdateArgs.Memory,
+		ComputeClass:    s.UpdateArgs.ComputeClass,
+	}
 }
