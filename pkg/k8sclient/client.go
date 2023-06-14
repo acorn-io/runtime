5db00b49215bca3b931b527a596bdca837ed24dc
Darren Shepherd <darren@acorn.io>
2023-06-14 16:54:03 -0700

    Remove direct lasso dependency
    Signed-off-by: Darren Shepherd <darren@acorn.io>

--- pkg/k8sclient/client.go
+++ pkg/k8sclient/client.go
@@ -3,7 +3,6 @@ package k8sclient
 import (
 	"github.com/acorn-io/acorn/pkg/scheme"
 	"github.com/acorn-io/baaah/pkg/restconfig"
-	"github.com/rancher/lasso/pkg/mapper"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -34,16 +33,7 @@ func DefaultInterface() (kubernetes.Interface, error) {
 }
 
 func New(cfg *rest.Config) (client.WithWatch, error) {
-	m, err := mapper.New(cfg)
-	if err != nil {
-		return nil, err
-	}
-	mapper, err := NewMapper(scheme.Scheme, m)
-	if err != nil {
-		return nil, err
-	}
 	return client.NewWithWatch(cfg, client.Options{
 		Scheme: scheme.Scheme,
-		Mapper: mapper,
 	})
 }
