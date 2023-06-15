ac95653ae7e6d85816143ef4ce92325248ba837a
tylerslaton <mtslaton1@gmail.com>
2023-06-15 16:09:25 -0400

    Add Project and Unlimited to Resources
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- pkg/apis/internal.admin.acorn.io/v1/resources.go
+++ pkg/apis/internal.admin.acorn.io/v1/resources.go
@@ -11,12 +11,15 @@ import (
 // external controllers to programmatically set the resources easier. Calls to
 // its functions are mutating.
 type Resources struct {
+	Unlimited bool `json:"unlimited,omitempty"`
+
 	Apps       int `json:"apps,omitempty"`
 	Containers int `json:"containers,omitempty"`
 	Jobs       int `json:"jobs,omitempty"`
 	Volumes    int `json:"volumes,omitempty"`
 	Secrets    int `json:"secrets,omitempty"`
 	Images     int `json:"images,omitempty"`
+	Projects   int `json:"projects,omitempty"`
 
 	VolumeStorage resource.Quantity `json:"volumeStorage,omitempty"`
 	Memory        resource.Quantity `json:"memory,omitempty"`
@@ -31,6 +34,7 @@ func (current *Resources) Add(incoming Resources) {
 	current.Volumes += incoming.Volumes
 	current.Secrets += incoming.Secrets
 	current.Images += incoming.Images
+	current.Projects += incoming.Projects
 
 	current.VolumeStorage.Add(incoming.VolumeStorage)
 	current.Memory.Add(incoming.Memory)
@@ -44,6 +48,7 @@ func (current *Resources) Remove(incoming Resources, all bool) {
 	current.Jobs -= incoming.Jobs
 	current.Volumes -= incoming.Volumes
 	current.Images -= incoming.Images
+	current.Projects -= incoming.Projects
 
 	current.Memory.Sub(incoming.Memory)
 	current.CPU.Sub(incoming.CPU)
@@ -60,87 +65,95 @@ func (current *Resources) Remove(incoming Resources, all bool) {
 // an aggregated error will be returned with all exceeded resources.
 func (current *Resources) Fits(incoming Resources) error {
 	exceededResources := []string{}
-	if current.Apps < incoming.Apps {
+	if current.Apps <= incoming.Apps {
 		exceededResources = append(exceededResources, "Apps")
 	}
-	if current.Containers < incoming.Containers {
+	if current.Containers <= incoming.Containers {
 		exceededResources = append(exceededResources, "Containers")
 	}
-	if current.Jobs < incoming.Jobs {
+	if current.Jobs <= incoming.Jobs {
 		exceededResources = append(exceededResources, "Jobs")
 	}
-	if current.Volumes < incoming.Volumes {
+	if current.Volumes <= incoming.Volumes {
 		exceededResources = append(exceededResources, "Volumes")
 	}
-	if current.Secrets < incoming.Secrets {
+	if current.Secrets <= incoming.Secrets {
 		exceededResources = append(exceededResources, "Secrets")
 	}
-	if current.Images < incoming.Images {
+	if current.Images <= incoming.Images {
 		exceededResources = append(exceededResources, "Images")
 	}
+	if current.Projects <= incoming.Projects {
+		exceededResources = append(exceededResources, "Projects")
+	}
 
-	if current.VolumeStorage.Cmp(incoming.VolumeStorage) < 0 {
+	if current.VolumeStorage.Cmp(incoming.VolumeStorage) <= 0 {
 		exceededResources = append(exceededResources, "VolumeStorage")
 	}
-	if current.Memory.Cmp(incoming.Memory) < 0 {
+	if current.Memory.Cmp(incoming.Memory) <= 0 {
 		exceededResources = append(exceededResources, "Memory")
 	}
-	if current.CPU.Cmp(incoming.CPU) < 0 {
+	if current.CPU.Cmp(incoming.CPU) <= 0 {
 		exceededResources = append(exceededResources, "Cpu")
 	}
 
 	// Build an aggregated error message for the exceeded resources
 	if len(exceededResources) > 0 {
-		return fmt.Errorf("exceeded quota for resources: %s", strings.Join(exceededResources, ", "))
+		return fmt.Errorf("quota would be exceeded for resources: %s", strings.Join(exceededResources, ", "))
 	}
 
 	return nil
 }
 
 // ToString will return a string representation of the Resources struct.
-func (current *Resources) ToString() string {
+func (current *Resources) NonEmptyString() string {
 	resources := []string{}
 	if current.Apps > 0 {
-		resources = append(resources, fmt.Sprintf("Apps:%v", current.Apps))
+		resources = append(resources, "Apps")
 	}
 	if current.Containers > 0 {
-		resources = append(resources, fmt.Sprintf("Containers:%v", current.Containers))
+		resources = append(resources, "Containers")
 	}
 	if current.Jobs > 0 {
-		resources = append(resources, fmt.Sprintf("Jobs:%v", current.Jobs))
+		resources = append(resources, "Jobs")
 	}
 	if current.Volumes > 0 {
-		resources = append(resources, fmt.Sprintf("Volumes:%v", current.Volumes))
+		resources = append(resources, "Volumes")
 	}
 	if current.Secrets > 0 {
-		resources = append(resources, fmt.Sprintf("Secrets:%v", current.Secrets))
+		resources = append(resources, "Secrets")
 	}
 	if current.Images > 0 {
-		resources = append(resources, fmt.Sprintf("Images:%v", current.Images))
+		resources = append(resources, "Images")
+	}
+	if current.Projects > 0 {
+		resources = append(resources, "Images")
 	}
 
 	if !current.VolumeStorage.IsZero() {
-		resources = append(resources, fmt.Sprintf("VolumeStorage:%v", current.VolumeStorage))
+		resources = append(resources, "VolumeStorage")
 	}
 	if !current.Memory.IsZero() {
-		resources = append(resources, fmt.Sprintf("Memory:%v", current.Memory))
+		resources = append(resources, "Memory")
 	}
 	if !current.CPU.IsZero() {
-		resources = append(resources, fmt.Sprintf("CPU:%v", current.CPU))
+		resources = append(resources, "CPU")
 	}
 
-	return strings.Join(resources, ",")
+	return strings.Join(resources, ", ")
 }
 
 // Equals will check if the current Resources struct is equal to another. This is useful
 // to avoid needing to do a deep equal on the entire struct.
 func (current *Resources) Equals(incoming Resources) bool {
-	return current.Apps == incoming.Apps &&
+	return current.Unlimited == incoming.Unlimited &&
+		current.Apps == incoming.Apps &&
 		current.Containers == incoming.Containers &&
 		current.Jobs == incoming.Jobs &&
 		current.Volumes == incoming.Volumes &&
 		current.Secrets == incoming.Secrets &&
 		current.Images == incoming.Images &&
+		current.Projects == incoming.Projects &&
 		current.VolumeStorage.Cmp(incoming.VolumeStorage) == 0 &&
 		current.Memory.Cmp(incoming.Memory) == 0 &&
 		current.CPU.Cmp(incoming.CPU) == 0
