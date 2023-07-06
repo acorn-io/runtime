b967c23c434bfc89a3bdc99b1671ea25bd692235
pratikjagrut <26519653+pratikjagrut@users.noreply.github.com>
2023-07-06 19:53:10 +0530

    feat: Create default secret when data section is empty in secret.yaml
    If secret.yaml does not include a data section, like the following:
    
    ```yaml
    apiVersion: api.acorn.io/v1
    kind: Secret
    metadata:
      name: staging-db-user-credentials2
      namespace: acorn
    type: basic
    ```
    
    When executing the command `kubectl create -f secret.yaml`, a secret will be created with a randomly generated username and password.
    
    Signed-off-by: pratikjagrut <26519653+pratikjagrut@users.noreply.github.com>

---
+++ pkg/server/registry/apigroups/acorn/secrets/defaultCreate.go
@@ -0,0 +1,43 @@
+package secrets
+
+import (
+	"context"
+
+	"github.com/acorn-io/mink/pkg/strategy"
+	"github.com/acorn-io/mink/pkg/types"
+	apiv1 "github.com/acorn-io/runtime/pkg/apis/api.acorn.io/v1"
+	sec "github.com/acorn-io/runtime/pkg/secrets"
+)
+
+type defaultSecretGenerateStrategy struct {
+	strategy strategy.CompleteStrategy
+}
+
+func (d *defaultSecretGenerateStrategy) Create(ctx context.Context, object types.Object) (types.Object, error) {
+	secret := object.(*apiv1.Secret)
+	// If the secret is of type 'basic' and data is empty,
+	// default username and password values are set.
+	if secret.Type == "basic" && secret.Data == nil {
+		username, err := sec.GenerateRandomSecret(8)
+		if err != nil {
+			return nil, err
+		}
+
+		password, err := sec.GenerateRandomSecret(16)
+		if err != nil {
+			return nil, err
+		}
+
+		if secret.Data == nil {
+			secret.Data = map[string][]byte{}
+		}
+
+		secret.Data["username"] = []byte(username)
+		secret.Data["password"] = []byte(password)
+	}
+	return d.strategy.Create(ctx, secret)
+}
+
+func (d *defaultSecretGenerateStrategy) New() types.Object {
+	return &apiv1.Secret{}
+}
