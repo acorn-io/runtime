9b4c567a81feac705b41fc37ef176ce46f088b6c
Thorsten Klein <tk@thklein.io>
2023-07-12 11:29:47 +0200

    add: acorn pull --verify -> do not pull image if signature verification failed
    Signed-off-by: Thorsten Klein <tk@thklein.io>

--- pkg/cli/pull.go
+++ pkg/cli/pull.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	cli "github.com/acorn-io/runtime/pkg/cli/builder"
 	"github.com/acorn-io/runtime/pkg/client"
 	"github.com/acorn-io/runtime/pkg/credentials"
@@ -19,7 +21,10 @@ func NewPull(c CommandContext) *cobra.Command {
 }
 
 type Pull struct {
-	client ClientFactory
+	client      ClientFactory
+	Verify      bool              `usage:"Verify the image signature BEFORE pulling and only pull on success" short:"v" local:"true" default:"false"`
+	Key         string            `usage:"Key to use for verifying" short:"k" local:"true" default:"./cosign.pub"`
+	Annotations map[string]string `usage:"Annotations to check for during verification" short:"a" local:"true" name:"annotation"`
 }
 
 func (s *Pull) Run(cmd *cobra.Command, args []string) error {
@@ -46,6 +51,17 @@ func (s *Pull) Run(cmd *cobra.Command, args []string) error {
 	auth, _, err := creds.Get(cmd.Context(), ref.Context().RegistryStr())
 	if err != nil {
 		return err
+	}
+
+	if s.Verify {
+		v := ImageVerify{
+			client:      s.client,
+			Key:         s.Key,
+			Annotations: s.Annotations,
+		}
+		if err := v.Run(cmd, args); err != nil {
+			return fmt.Errorf("NOT pulling image: %w", err)
+		}
 	}
 
 	progress, err := c.ImagePull(cmd.Context(), args[0], &client.ImagePullOptions{
