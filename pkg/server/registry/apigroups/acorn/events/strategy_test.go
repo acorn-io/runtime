94f480bc6525ff824e43c2bf4fe47b1d83b846d4
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-07-26 02:50:58 -0400

    Add unit tests for event tail option
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/server/registry/apigroups/acorn/events/strategy_test.go
+++ pkg/server/registry/apigroups/acorn/events/strategy_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	apiv1 "github.com/acorn-io/runtime/pkg/apis/api.acorn.io/v1"
 	internalv1 "github.com/acorn-io/runtime/pkg/apis/internal.acorn.io/v1"
 	"github.com/acorn-io/z"
 	"github.com/stretchr/testify/assert"
@@ -63,6 +64,98 @@ func TestParseTimeBound(t *testing.T) {
 				return
 			}
 			assert.Equal(t, tt.want.parsed, parsed)
+		})
+	}
+}
+
+func TestQueryFilter(t *testing.T) {
+	ts := internalv1.NowMicro()
+	tests := []struct {
+		name  string
+		query query
+		args  []apiv1.Event
+		want  []apiv1.Event
+	}{
+		{
+			name: "Tail less than length",
+			query: query{
+				tail: 1,
+			},
+			args: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+			want: []apiv1.Event{
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+		},
+		{
+			name: "Tail more than length",
+			query: query{
+				tail: 3,
+			},
+			args: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+			want: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+		},
+		{
+			name: "Tail since",
+			query: query{
+				tail:  2,
+				since: z.Pointer(internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))),
+			},
+			args: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+			want: []apiv1.Event{
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+		},
+		{
+			name: "Tail until",
+			query: query{
+				tail:  2,
+				until: z.Pointer(internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))),
+			},
+			args: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+				{Observed: internalv1.NewMicroTime(ts.Add(2 * time.Microsecond))},
+			},
+			want: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+			},
+		},
+		{
+			name: "Tail window",
+			query: query{
+				tail:  2,
+				since: z.Pointer(internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))),
+				until: z.Pointer(internalv1.NewMicroTime(ts.Add(3 * time.Microsecond))),
+			},
+			args: []apiv1.Event{
+				{Observed: ts},
+				{Observed: internalv1.NewMicroTime(ts.Add(1 * time.Microsecond))},
+				{Observed: internalv1.NewMicroTime(ts.Add(2 * time.Microsecond))},
+				{Observed: internalv1.NewMicroTime(ts.Add(3 * time.Microsecond))},
+				{Observed: internalv1.NewMicroTime(ts.Add(4 * time.Microsecond))},
+			},
+			want: []apiv1.Event{
+				{Observed: internalv1.NewMicroTime(ts.Add(2 * time.Microsecond))},
+				{Observed: internalv1.NewMicroTime(ts.Add(3 * time.Microsecond))},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.want, tt.query.filter(tt.args...))
 		})
 	}
 }
