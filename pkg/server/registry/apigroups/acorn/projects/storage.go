093e76f8fe2f0d0aa5ebedc17689d3ec1b8c4488
Donnie Adams <donnie@acorn.io>
2023-08-28 14:21:51 -0400

    Migrate legacy project namespaces
    If a project namespace existing prior to upgrading to the latest
    version, then the namespace needs to be migration to a new project
    instance. This change does this by using a proper label selector to find
    namespaces that need to be migrated.
    
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/server/registry/apigroups/acorn/projects/storage.go
+++ pkg/server/registry/apigroups/acorn/projects/storage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/acorn-io/baaah/pkg/router"
 	"github.com/acorn-io/mink/pkg/stores"
@@ -18,12 +19,18 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	klabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/apiserver/pkg/registry/rest"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func NewStorage(c kclient.WithWatch, namespaceCheck bool) rest.Storage {
+func NewStorage(c kclient.WithWatch, namespaceCheck bool) (rest.Storage, error) {
+	if err := migrateLegacyNamespaces(context.Background(), c); err != nil {
+		return nil, err
+	}
+
 	remoteResource := translation.NewSimpleTranslationStrategy(&translator{}, remote.NewRemote(&v1.ProjectInstance{}, c))
 	validator := &Validator{Client: c}
 	return stores.NewBuilder(c.Scheme(), &apiv1.Project{}).
@@ -32,7 +39,7 @@ func NewStorage(c kclient.WithWatch, namespaceCheck bool) rest.Storage {
 		WithValidateCreate(validator).
 		WithValidateUpdate(validator).
 		WithTableConverter(tables.ProjectConverter).
-		Build()
+		Build(), nil
 }
 
 type projectCreater struct {
@@ -76,4 +83,41 @@ func (pr *projectCreater) Create(ctx context.Context, object types.Object) (type
 	}
 
 	return pr.creater.Create(ctx, object)
+}
+
+func migrateLegacyNamespaces(ctx context.Context, c kclient.Client) error {
+	// Once a project namespace is managed by the project instance, then this label will be set on it.
+	// Therefore, using this label selector ensures the migration only happens once for a namespace.
+	notManaged, err := klabels.NewRequirement(labels.AcornManaged, selection.DoesNotExist, nil)
+	if err != nil {
+		return err
+	}
+
+	nsSelector := klabels.SelectorFromSet(map[string]string{
+		labels.AcornProject: "true",
+	}).Add(*notManaged)
+	namespaces := corev1.NamespaceList{}
+	if err := c.List(ctx, &namespaces, kclient.MatchingLabelsSelector{Selector: nsSelector}); err != nil {
+		return err
+	}
+
+	for _, ns := range namespaces.Items {
+		project := &v1.ProjectInstance{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: ns.Name,
+			},
+			Spec: v1.ProjectInstanceSpec{
+				DefaultRegion: ns.Annotations[labels.AcornProjectDefaultRegion],
+			},
+		}
+
+		if supportedRegions := ns.Annotations[labels.AcornProjectSupportedRegions]; supportedRegions != "" {
+			project.Spec.SupportedRegions = strings.Split(supportedRegions, ",")
+		}
+		if err = c.Create(ctx, project); err != nil && !apierrors.IsAlreadyExists(err) {
+			return err
+		}
+	}
+
+	return nil
 }
