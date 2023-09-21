0cb4981c2a5523dd3fa10c3d72e135753d7c8ae5
Thorsten Klein <tk@thklein.io>
2023-09-21 18:29:48 +0200

    change: don't consider parent/child acorns when checking for authorized permissions
    Signed-off-by: Thorsten Klein <tk@thklein.io>

--- integration/client/imagerules/imageroleauthorizations_test.go
+++ integration/client/imagerules/imageroleauthorizations_test.go
@@ -2,6 +2,8 @@ package imagerules
 
 import (
 	"context"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/acorn-io/runtime/integration/helper"
@@ -14,12 +16,16 @@ import (
 	"github.com/acorn-io/runtime/pkg/imageselector"
 	kclient "github.com/acorn-io/runtime/pkg/k8sclient"
 	"github.com/acorn-io/runtime/pkg/profiles"
-	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/stretchr/testify/require"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	cclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	_ "embed"
 )
+
+//go:embed testdata/nested-perms/Acornfile
+var parentAcornfile []byte
 
 func TestImageRoleAuthorizations(t *testing.T) {
 	helper.StartController(t)
@@ -43,8 +49,50 @@ func TestImageRoleAuthorizations(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Build nested image
+	nestedImage, err := c.AcornImageBuild(ctx, "./testdata/nested-perms/nested.acorn", &client.AcornImageBuildOptions{
+		Cwd: "./testdata/nested-perms/",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	nestedImageID := nestedImage.ID
+
+	nestedImageTagName := registry + "/test:ci-nested"
+
+	err = c.ImageTag(ctx, nestedImageID, nestedImageTagName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	progress, err := c.ImagePush(ctx, nestedImageTagName, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for update := range progress {
+		if update.Error != "" {
+			t.Fatal(update.Error)
+		}
+	}
+
+	// Copy Acornfile and replace image name with the nested image
+	acornfile, err := os.CreateTemp("testdata/nested-perms", "test-acornfile-*.acorn")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = acornfile.Write([]byte(strings.Replace(string(parentAcornfile), "%REPLACE_IMAGE%", nestedImageTagName, 1)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		os.Remove(acornfile.Name())
+	})
+
 	// Build Image
-	image, err := c.AcornImageBuild(ctx, "./testdata/nested-perms/Acornfile", &client.AcornImageBuildOptions{
+	image, err := c.AcornImageBuild(ctx, acornfile.Name(), &client.AcornImageBuildOptions{
 		Cwd: "./testdata/nested-perms/",
 	})
 	if err != nil {
@@ -59,7 +107,7 @@ func TestImageRoleAuthorizations(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	progress, err := c.ImagePush(ctx, tagName, nil)
+	progress, err = c.ImagePush(ctx, tagName, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -76,16 +124,6 @@ func TestImageRoleAuthorizations(t *testing.T) {
 	}
 	require.Equal(t, 1, len(details.NestedImages))
 
-	ref, err := name.ParseReference(tagName)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	targetDigest := ref.Context().Digest(details.AppImage.Digest)
-	t.Logf("target digest: %s", targetDigest)
-
-	require.Empty(t, details.SignatureDigest, "signature digest should be empty before signing")
-
 	// DEPLOY
 
 	// Integration tests don't have proper privileges so we will by pass the permission validation
@@ -95,13 +133,13 @@ func TestImageRoleAuthorizations(t *testing.T) {
 			Namespace:    c.GetProject(),
 		},
 		Spec: internalv1.AppInstanceSpec{
-			Image: image.ID,
+			Image: tagName,
 			Permissions: []internalv1.Permissions{
 				{
 					ServiceName: "rootapp",
 					Rules: []internalv1.PolicyRule{{
 						PolicyRule: rbacv1.PolicyRule{
-							APIGroups: []string{"api.acorn.io"},
+							APIGroups: []string{"foo.bar.com"},
 							Verbs:     []string{"get"},
 						},
 					}},
@@ -133,8 +171,6 @@ func TestImageRoleAuthorizations(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		t.Logf("Creating app %s", app.Name)
-
 		helper.WaitForObject(t, helper.Watcher(t, c), &apiv1.AppList{}, app, func(obj *apiv1.App) bool {
 			return obj.Status.Staged.PermissionsChecked
 		})
@@ -143,8 +179,6 @@ func TestImageRoleAuthorizations(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-
-		t.Logf("Created app %s", app.Name)
 
 		return app
 	}
@@ -156,11 +190,43 @@ func TestImageRoleAuthorizations(t *testing.T) {
 		}
 	}
 
+	// Prep: Create a role that we can use later
+	role := &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo:bar:admin",
+			Namespace: c.GetNamespace(),
+		},
+		Rules: []rbacv1.PolicyRule{{
+			APIGroups: []string{"foo.bar.com"},
+			Verbs:     []string{"*"},
+			Resources: []string{"*"},
+		}},
+	}
+
+	err = kclient.Create(ctx, role)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	// --------------------------------------------------------------------
 	// Run #1 - Expect denied permissions since we have no IRAs
 	// --------------------------------------------------------------------
 	app := createWaitLoop(ctx, blueprint.DeepCopy())
-	require.Equal(t, 2, len(app.Status.Staged.ImagePermissionsDenied), "should have 2 denied permissions with no IRA defined")
+	require.Equal(t, 1, len(app.Status.Staged.ImagePermissionsDenied), "should have 1 denied permission (rootapp) with no IRA defined")
+	expectedDeniedPermissionsRootapp := []internalv1.Permissions{
+		{
+			ServiceName: tagName,
+			Rules: []internalv1.PolicyRule{
+				{
+					PolicyRule: rbacv1.PolicyRule{
+						APIGroups: []string{"foo.bar.com"},
+						Verbs:     []string{"get"},
+					},
+				},
+			},
+		},
+	}
+	require.Equal(t, expectedDeniedPermissionsRootapp, app.Status.Staged.ImagePermissionsDenied, "should have denied permissions for foo.bar.com on rootapp")
 	rmapp(ctx, app)
 
 	// --------------------------------------------------------------------
@@ -178,8 +244,8 @@ func TestImageRoleAuthorizations(t *testing.T) {
 			Scopes: []string{"project"},
 			RoleRefs: []internaladminv1.RoleRef{
 				{
-					Name: "acorn:aws:admin",
-					Kind: "ClusterRole",
+					Name: "foo:bar:admin", // required by rootapp
+					Kind: "Role",          // current namespace only
 				},
 			},
 		},
@@ -191,7 +257,7 @@ func TestImageRoleAuthorizations(t *testing.T) {
 	}
 
 	app = createWaitLoop(ctx, blueprint.DeepCopy())
-	require.Equal(t, 2, len(app.Status.Staged.ImagePermissionsDenied), "should have 2 denied permissions")
+	require.Equal(t, 1, len(app.Status.Staged.ImagePermissionsDenied), "should have 1 denied permission (rootapp)")
 	rmapp(ctx, app)
 
 	// --------------------------------------------------------------------
@@ -214,168 +280,79 @@ func TestImageRoleAuthorizations(t *testing.T) {
 
 	ra, err := imagerules.GetAuthorizedPermissions(ctx, kclient, c.GetNamespace(), tagName, image.Digest)
 	require.NoError(t, err, "should not error while getting authorized permissions")
-	t.Logf("Authorized Permissions: %#v", ra)
 
 	// replace name in granted permissions
 	for i := range ra {
-		ra[i].ServiceName = "foo.awsapp"
+		ra[i].ServiceName = "rootapp"
 	}
 
-	missing, granted := internalv1.GrantsAll(c.GetNamespace(), details.NestedImages[0].Permissions, ra)
-	require.True(t, granted, "should have granted permissions, but have missing: %#v\nr: %#v\ng:%#v", missing, details.NestedImages[0].Permissions, ra)
+	missing, granted := internalv1.GrantsAll(c.GetNamespace(), details.Permissions, ra)
+	require.True(t, granted, "should have granted permissions, but have missing: %#v\nr: %#v\ng:%#v", missing, details.Permissions, ra)
 
 	app = createWaitLoop(ctx, blueprint.DeepCopy())
-	require.Equal(t, 1, len(app.Status.Staged.ImagePermissionsDenied), "should have 1 denied permissions: %#v", app.Status.Staged.ImagePermissionsDenied)
+	require.Equal(t, 0, len(app.Status.Staged.ImagePermissionsDenied), "should have 1 denied permissions: %#v", app.Status.Staged.ImagePermissionsDenied)
+
+	app = helper.WaitForObject(t, helper.Watcher(t, c), &apiv1.AppList{}, app, func(obj *apiv1.App) bool {
+		return obj.Status.AppStatus.Containers["rootapp"].Ready
+	})
+
+	// When parent app is ready, the nested app should not, because it still has denied permissions
+	nestedApp, err := c.AppGet(ctx, app.Status.AppStatus.Acorns["foo"].AcornName)
+	require.NoError(t, err, "should not error while getting nested app")
+
+	nestedApp = helper.WaitForObject(t, helper.Watcher(t, c), &apiv1.AppList{}, nestedApp, func(obj *apiv1.App) bool {
+		return obj.Status.Staged.PermissionsChecked
+	})
+
+	require.Equal(t, 1, len(nestedApp.Status.Staged.ImagePermissionsDenied), "should have 1 denied permission (foo.awsapp)")
+	expectedDeniedPermissionsFooAwsapp := []internalv1.Permissions{
+		{
+			ServiceName: nestedImageTagName,
+			Rules: []internalv1.PolicyRule{
+				{
+					PolicyRule: rbacv1.PolicyRule{
+						APIGroups: []string{"aws.acorn.io"},
+						Verbs:     []string{"get"},
+					},
+				},
+			},
+		},
+	}
+
+	require.Equal(t, expectedDeniedPermissionsFooAwsapp, nestedApp.Status.Staged.ImagePermissionsDenied, "should have denied permissions for aws.acorn.io on foo.awsapp")
+
 	rmapp(ctx, app)
 
 	// --------------------------------------------------------------------
+	// Run #4 - Expect no denied permissions since we have an IRA that covers the image
+	// --------------------------------------------------------------------
 
-	// // create first rule
+	// Add the missing api group to the IRA
+	ira.ImageSelector.NamePatterns = []string{tagName, nestedImageTagName}
+	ira.Roles.RoleRefs = append(ira.Roles.RoleRefs, internaladminv1.RoleRef{
+		Name: "acorn:aws:admin", // required by foo.awsapp
+		Kind: "ClusterRole",     // current namespace only
+	})
+	err = kclient.Update(ctx, ira)
+	require.NoError(t, err, "should not error while updating IRA")
 
-	// // update image allow rule to cover that image
-	// ira.ImageSelector.NamePatterns = []string{tagName, id}
+	// Ensure that the selector matches the image now
+	err = imageselector.MatchImage(ctx, kclient, c.GetNamespace(), nestedImageTagName, "", nestedImage.Digest, ira.ImageSelector)
+	require.NoError(t, err, "should not error while matching image")
 
-	// err = kclient.Update(ctx, ira)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	app = createWaitLoop(ctx, blueprint.DeepCopy())
+	require.Equal(t, 0, len(app.Status.Staged.ImagePermissionsDenied), "should have 0 denied permissions")
 
-	// var niar apiv1.ImageAllowRule
-	// err = kclient.Get(ctx, cclient.ObjectKeyFromObject(ira), &niar)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	app = helper.WaitForObject(t, helper.Watcher(t, c), &apiv1.AppList{}, app, func(obj *apiv1.App) bool {
+		return obj.Status.AppStatus.Containers["rootapp"].Ready
+	})
 
-	// require.Equal(t, ira.ImageSelector.NamePatterns, niar.ImageSelector.NamePatterns)
+	nestedApp, err = c.AppGet(ctx, app.Status.AppStatus.Acorns["foo"].AcornName)
+	require.NoError(t, err, "should not error while getting nested app")
 
-	// // try to run by tagName - expect success
-	// app, err = c.AppRun(ctx, tagName, nil)
-	// require.NoError(t, err, "should not error since image `%s` is covered by images scope `%+v` of IAR and there are not other rules", tagName, ira.ImageSelector.NamePatterns)
+	nestedApp = helper.WaitForObject(t, helper.Watcher(t, c), &apiv1.AppList{}, nestedApp, func(obj *apiv1.App) bool {
+		return obj.Status.Staged.PermissionsChecked
+	})
 
-	// // remove app
-	// _, err = c.AppDelete(ctx, app.Name)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // try to run by ID - expect success
-	// app, err = c.AppRun(ctx, id, nil)
-	// require.NoError(t, err, "should not error since image `%s` is covered by images scope `%+v` of IAR and there are not other rules", id, ira.ImageSelector.NamePatterns)
-
-	// // remove app
-	// _, err = c.AppDelete(ctx, app.Name)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // update iar to require a signature
-	// ira.ImageSelector.Signatures = []internalv1.SignatureRules{
-	// 	{
-	// 		SignedBy: internalv1.SignedBy{
-	// 			AllOf: []string{string(pubkeyCosign)},
-	// 		},
-	// 	},
-	// }
-
-	// err = kclient.Update(ctx, ira)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // try to run - expect failure
-	// _, err = c.AppRun(ctx, tagName, nil)
-	// require.Error(t, err, "should error since image %s is not signed by the required key", tagName)
-
-	// // sign image
-	// nsig, err := signImage(ctx, c, targetDigest, tagName, "./testdata/cosign.key")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // push again (this should include the signature)
-	// progress, err = c.ImagePush(ctx, tagName, nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// for update := range progress {
-	// 	if update.Error != "" {
-	// 		t.Fatal(update.Error)
-	// 	}
-	// }
-
-	// require.NotEmpty(t, nsig.SignatureDigest, "signature should not be empty")
-
-	// ndetails, err := c.ImageDetails(ctx, tagName, nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// t.Logf("Image %s has signature %s", tagName, ndetails.SignatureDigest)
-	// require.NotEmpty(t, ndetails.SignatureDigest, "signature digest should not be empty after signing")
-
-	// // try to run by tagName - expect success
-	// app, err = c.AppRun(ctx, tagName, nil)
-	// require.NoError(t, err, "should not error since image %s is signed by the required key", tagName)
-
-	// // remove app
-	// _, err = c.AppDelete(ctx, app.Name)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // try to run by ID - expect success
-	// app, err = c.AppRun(ctx, id, nil)
-	// require.NoError(t, err, "should not error since image %s is signed by the required key", id)
-
-	// // remove app
-	// _, err = c.AppDelete(ctx, app.Name)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // Delete the image, so that it's only in the external registry
-	// img, tags, err := c.ImageDelete(ctx, id, nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// require.Contains(t, tags, tagName, "tag %s should be deleted", tagName)
-	// require.Equal(t, id, img.Name, "image %s should be deleted", id)
-
-	// // try to run by tagName - expect success
-	// app, err = c.AppRun(ctx, tagName, nil)
-	// require.NoError(t, err, "should not error since image %s is signed by the required key", tagName)
-
-	// // remove app
-	// _, err = c.AppDelete(ctx, app.Name)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // try to run by id - expect failure
-	// _, err = c.AppRun(ctx, id, nil)
-	// require.Error(t, err, "should error since image %s was deleted", id)
-
-	// // update iar to require a signature with specific annotation
-	// ira.ImageSelector.Signatures = []internalv1.SignatureRules{
-	// 	{
-	// 		SignedBy: internalv1.SignedBy{
-	// 			AllOf: []string{string(pubkeyCosign)},
-	// 		},
-	// 		Annotations: internalv1.SignatureAnnotations{
-	// 			Match: map[string]string{
-	// 				"foo": "bar",
-	// 			},
-	// 		},
-	// 	},
-	// }
-
-	// err = kclient.Update(ctx, ira)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// // try to run - expect failure
-	// _, err = c.AppRun(ctx, tagName, nil)
-	// require.Error(t, err, "should error since image is signed by the required key but does not match the required annotation")
+	require.Equal(t, 0, len(nestedApp.Status.Staged.ImagePermissionsDenied), "should have 0 denied permissions")
 }
