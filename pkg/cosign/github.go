8971294d27bc4b5f278b7f6449479d57c5caaa9e
Thorsten Klein <tk@thklein.io>
2023-09-26 07:38:03 +0200

    change: some more error handling for fetching pubkeys from gh API
    - this will surface rate-limiting errors to the administrators via
      controller logs
    
    Signed-off-by: Thorsten Klein <tk@thklein.io>

--- pkg/cosign/github.go
+++ pkg/cosign/github.go
@@ -17,11 +17,21 @@ type GitHubPublicKey struct {
 func getGitHubPublicKeys(username string) ([]GitHubPublicKey, error) {
 	logrus.Debugf("Getting public keys for GitHub user %s", username)
 	url := fmt.Sprintf("https://api.github.com/users/%s/keys", username)
-	resp, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("unexpected status code %d (%s) from GitHub API: %s", resp.StatusCode, resp.Status, string(body))
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
