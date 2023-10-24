fd5a12dd651a9a05caba8e8e45f2f1806d42a79c
Oscar Ward <oscar@acorn.io>
2023-10-24 12:16:55 -0700

    add comment and test
    Signed-off-by: Oscar Ward <oscar@acorn.io>

--- pkg/cli/rm_helper.go
+++ pkg/cli/rm_helper.go
@@ -23,6 +23,7 @@ func getSecretsToRemove(arg string, client client.Client, cmd *cobra.Command) ([
 	}
 
 	for _, secret := range secrets {
+		// We only want to delete secrets for the given app and not nested apps
 		if after, found := strings.CutPrefix(secret.Name, arg+"."); found && !strings.ContainsRune(after, '.') {
 			result = append(result, secret.Name)
 		}
