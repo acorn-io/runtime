524b8e17ffab888a778832d20ad39347dda41b53
Grant Linville <grant@acorn.io>
2024-01-18 15:50:17 -0500

    use updated app generation instead
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/log/default_log.go
+++ pkg/log/default_log.go
@@ -3,7 +3,6 @@ package log
 import (
 	"context"
 	"sync"
-	"time"
 
 	apiv1 "github.com/acorn-io/runtime/pkg/apis/api.acorn.io/v1"
 	"github.com/acorn-io/runtime/pkg/client"
@@ -27,7 +26,6 @@ type DefaultLoggerImpl struct {
 	client              client.Client
 	containerColors     map[string]pterm.Color
 	lastLoginGeneration int64
-	lastLoginTime       time.Time
 }
 
 func (d *DefaultLoggerImpl) Errorf(format string, args ...interface{}) {
@@ -48,26 +46,14 @@ func (d *DefaultLoggerImpl) AppStatus(ready bool, msg string, app *apiv1.App) {
 	} else {
 		d.lock.Lock()
 		defer d.lock.Unlock()
-
-		// Prompt the user to fill in a credential secret if:
-		// - The app status indicates a login is needed, AND
-		// - The app has been updated since the last time the user logged in, AND
-		// - (The app is running locally OR the last login was more than 5 seconds ago)
-		// The last condition is needed to avoid a race condition in the SaaS where the user is prompted multiple
-		// times in a row to fill in a credential secret. We don't want to prompt a user more than once within
-		// five seconds in order to avoid the double prompt.
-		if app.Status.AppStatus.LoginRequired &&
-			app.Status.ObservedGeneration > d.lastLoginGeneration &&
-			(app.Status.ResolvedOfferings.Region == apiv1.LocalRegion ||
-				time.Since(d.lastLoginTime) > 5*time.Second) {
-			if err := login.Secrets(d.ctx, d.client, app); err != nil {
+		if app.Status.AppStatus.LoginRequired && app.Status.ObservedGeneration > d.lastLoginGeneration {
+			updatedApp, err := login.Secrets(d.ctx, d.client, app)
+			if err != nil {
 				go d.Errorf(err.Error())
 			} else {
-				d.lastLoginGeneration = app.Generation
-				d.lastLoginTime = time.Now()
+				d.lastLoginGeneration = updatedApp.Generation
 			}
 		}
-
 		pterm.Println(pterm.LightYellow(msg))
 	}
 }
