6e9cb0df71b825eb1be33aa6bd0d321a41da63cd
Darren Shepherd <darren@acorn.io>
2024-01-22 22:00:27 -0700

    Handle unknown and evicted pods
    Signed-off-by: Darren Shepherd <darren@acorn.io>

--- pkg/controller/local/pods.go
+++ pkg/controller/local/pods.go
@@ -18,5 +18,23 @@ func DeletePods(req router.Request, resp router.Response) error {
 			return req.Client.Delete(req.Ctx, pod)
 		}
 	}
+
+	if pod.Status.Phase == corev1.PodRunning && len(pod.Status.ContainerStatuses) > 0 {
+		allUnknown := true
+		for _, status := range pod.Status.ContainerStatuses {
+			if status.State.Terminated == nil || status.State.Terminated.Reason != "Unknown" {
+				allUnknown = false
+				break
+			}
+		}
+		if allUnknown {
+			return req.Client.Delete(req.Ctx, pod)
+		}
+	}
+
+	if pod.Status.Reason == "Evicted" {
+		return req.Client.Delete(req.Ctx, pod)
+	}
+
 	return nil
 }
