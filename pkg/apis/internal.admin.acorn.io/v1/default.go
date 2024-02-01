3eb5193496ef406620f3198d7464c4d7bde3c4a4
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-01-31 21:41:41 -0500

    enhance: factor project default cc into default cc selection
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/apis/internal.admin.acorn.io/v1/default.go
+++ pkg/apis/internal.admin.acorn.io/v1/default.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"sort"
 
+	internalv1 "github.com/acorn-io/runtime/pkg/apis/internal.acorn.io/v1"
+	"github.com/acorn-io/z"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func getCurrentClusterComputeClassDefault(ctx context.Context, c client.Client) (*ClusterComputeClassInstance, error) {
+func getCurrentClusterComputeClassDefault(ctx context.Context, c client.Client, projectDefaultComputeClass string) (*ClusterComputeClassInstance, error) {
 	clusterComputeClasses := ClusterComputeClassInstanceList{}
 	if err := c.List(ctx, &clusterComputeClasses, &client.ListOptions{}); err != nil {
 		return nil, err
@@ -18,7 +20,7 @@ func getCurrentClusterComputeClassDefault(ctx context.Context, c client.Client) 
 		return clusterComputeClasses.Items[i].Name < clusterComputeClasses.Items[j].Name
 	})
 
-	var defaultCCC *ClusterComputeClassInstance
+	var defaultCCC, projectDefaultCCC *ClusterComputeClassInstance
 	for _, clusterComputeClass := range clusterComputeClasses.Items {
 		if clusterComputeClass.Default {
 			if defaultCCC != nil {
@@ -26,15 +28,26 @@ func getCurrentClusterComputeClassDefault(ctx context.Context, c client.Client) 
 					"cannot establish defaults because two default computeclasses exist: %v and %v",
 					defaultCCC.Name, clusterComputeClass.Name)
 			}
-			t := clusterComputeClass // Create a new variable that isn't being iterated on to get a pointer
-			defaultCCC = &t
+
+			// Create a new variable that isn't being iterated on to get a pointer
+			if projectDefaultComputeClass != "" {
+				defaultCCC = z.Pointer(clusterComputeClass)
+			}
 		}
+
+		if clusterComputeClass.Name == projectDefaultComputeClass {
+			projectDefaultCCC = z.Pointer(clusterComputeClass)
+		}
+	}
+
+	if projectDefaultCCC != nil {
+		return projectDefaultCCC, nil
 	}
 
 	return defaultCCC, nil
 }
 
-func getCurrentProjectComputeClassDefault(ctx context.Context, c client.Client, namespace string) (*ProjectComputeClassInstance, error) {
+func getCurrentProjectComputeClassDefault(ctx context.Context, c client.Client, projectDefaultComputeClass, namespace string) (*ProjectComputeClassInstance, error) {
 	projectComputeClasses := ProjectComputeClassInstanceList{}
 	if err := c.List(ctx, &projectComputeClasses, &client.ListOptions{Namespace: namespace}); err != nil {
 		return nil, err
@@ -44,7 +57,7 @@ func getCurrentProjectComputeClassDefault(ctx context.Context, c client.Client, 
 		return projectComputeClasses.Items[i].Name < projectComputeClasses.Items[j].Name
 	})
 
-	var defaultPCC *ProjectComputeClassInstance
+	var defaultPCC, projectDefaultPCC *ProjectComputeClassInstance
 	for _, projectComputeClass := range projectComputeClasses.Items {
 		if projectComputeClass.Default {
 			if defaultPCC != nil {
@@ -52,23 +65,40 @@ func getCurrentProjectComputeClassDefault(ctx context.Context, c client.Client, 
 					"cannot establish defaults because two default computeclasses exist: %v and %v",
 					defaultPCC.Name, projectComputeClass.Name)
 			}
-			t := projectComputeClass // Create a new variable that isn't being iterated on to get a pointer
-			defaultPCC = &t
+
+			// Create a new variable that isn't being iterated on to get a pointer
+			if projectDefaultComputeClass != "" {
+				defaultPCC = z.Pointer(projectComputeClass)
+			}
 		}
+
+		if projectComputeClass.Name == projectDefaultComputeClass {
+			projectDefaultPCC = z.Pointer(projectComputeClass)
+		}
+	}
+
+	if projectDefaultPCC != nil {
+		return projectDefaultPCC, nil
 	}
 
 	return defaultPCC, nil
 }
 
 func GetDefaultComputeClass(ctx context.Context, c client.Client, namespace string) (string, error) {
-	pcc, err := getCurrentProjectComputeClassDefault(ctx, c, namespace)
+	var project internalv1.ProjectInstance
+	if err := c.Get(ctx, client.ObjectKey{Name: namespace}, &project); err != nil {
+		return "", fmt.Errorf("failed to get projectinstance to determine default compute class: %w", err)
+	}
+	projectDefault := project.Status.DefaultComputeClass
+
+	pcc, err := getCurrentProjectComputeClassDefault(ctx, c, projectDefault, namespace)
 	if err != nil {
 		return "", err
 	} else if pcc != nil {
 		return pcc.Name, nil
 	}
 
-	ccc, err := getCurrentClusterComputeClassDefault(ctx, c)
+	ccc, err := getCurrentClusterComputeClassDefault(ctx, c, projectDefault)
 	if err != nil {
 		return "", err
 	} else if ccc != nil {
--- pkg/project/handlers.go
+++ pkg/project/handlers.go
@@ -1,11 +1,13 @@
 package project
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/acorn-io/baaah/pkg/router"
 	apiv1 "github.com/acorn-io/runtime/pkg/apis/api.acorn.io/v1"
 	v1 "github.com/acorn-io/runtime/pkg/apis/internal.acorn.io/v1"
+	"github.com/acorn-io/runtime/pkg/computeclasses"
 	"github.com/acorn-io/runtime/pkg/labels"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -20,7 +22,21 @@ func SetDefaultComputeClass(req router.Request, resp router.Response) error {
 	project := req.Object.(*v1.ProjectInstance)
 	if cc := project.Spec.DefaultComputeClass; cc != "" &&
 		project.Status.DefaultComputeClass != cc {
+		// The spec has been changed, update the status field to match.
 		project.Status.DefaultComputeClass = cc
+	}
+
+	// Check if the given compute class exists
+	if project.Status.DefaultComputeClass != "" {
+		if _, err := computeclasses.GetAsProjectComputeClassInstance(req.Ctx, req.Client, project.Status.Namespace, project.Status.DefaultComputeClass); err != nil {
+			if !apierrors.IsNotFound(err) {
+				return fmt.Errorf("failed to check existence of default compute class on project [%s] status: %w", project.Name, err)
+			}
+
+			// The compute class does not exist, clear the status field.
+			project.Status.DefaultComputeClass = ""
+		}
+		// TODO(njhale): Unset the status field if the project does not support the same regions as the compute class?
 	}
 
 	resp.Objects(req.Object)
